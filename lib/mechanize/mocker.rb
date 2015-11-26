d35c77d365b40d00baa9898e4794d045651c4dd2
Mariano Olmos <molmos@despegar.com>
2015-11-26 15:10:29 -0300

    first commit

---
+++ lib/mechanize/mocker.rb
@@ -0,0 +1,7 @@
+require "mechanize/mocker/version"
+
+module Mechanize
+  module Mocker
+    # Your code goes here...
+  end
+end
---
+++ lib/mechanize/mocker/version.rb
@@ -0,0 +1,5 @@
+module Mechanize
+  module Mocker
+    VERSION = "0.1.0"
+  end
+end
